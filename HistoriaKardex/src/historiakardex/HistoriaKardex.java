3b55a066937307d35f95f2b0897397287814f19a
javierFili <javierfiligranaagreda@gmail.com>
2021-04-30 23:21:31 -0400

    La mayoria de las consultas ya esta, pero falta la de devolver el resultado despues del visto bueno de administrador,y creo que te tengo una falla al enviar el tipo de dato,date en fecha.

--- HistoriaKardex/src/historiakardex/HistoriaKardex.java
+++ HistoriaKardex/src/historiakardex/HistoriaKardex.java
@@ -11,11 +11,13 @@ package historiakardex;
  */
 public class HistoriaKardex {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
     
 }
